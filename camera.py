5db27bf0b5e83906538c16cdffbe1a18c342e23d
salieu <sowealieu@gmail.com>
2019-12-10 12:08:57 +0100

    added video recording in task6environment.js file

--- camera.py
+++ camera.py
@@ -2,8 +2,6 @@ from picamera import PiCamera
 from time import sleep
 
 camera = PiCamera()
-camera.start_preview()
-for i in range(2):
-    sleep(10)
-    camera.capture('/home/pi/Desktop/task6/image%s.jpg'% i)
-camera.stop_preview()
+camera.start_recording()
+    camera.capture('/home/pi/Desktop/task6/video.h264)
+camera.stop_recording()
