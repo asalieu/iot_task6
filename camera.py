0b9ea062d336a40cbfd5eb3f826ecee24c24cd66
asalieu <54984697+asalieu@users.noreply.github.com>
2019-12-16 18:11:41 +0100

    updated video python recording

--- camera.py
+++ camera.py
@@ -1,7 +1,14 @@
+import os
+import glob
 from picamera import PiCamera
 from time import sleep
 
-camera = PiCamera()
-camera.start_recording()
-    camera.capture('/home/pi/Desktop/task6/video.h264)
-camera.stop_recording()
+#fpath ="C:\\Users\\C#\\demofile.py";
+
+if os.path.isfile("video.h264"):
+  os.remove("video.h264")  
+else:    
+  camera.start_recording()
+  camera.capture('/home/pi/Desktop/task6/video.h264)
+  camera.stop_recording()
+  camera = PiCamera()  
